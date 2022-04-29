17603de68917ea559cc92bc4fd46e92acfd96c2b
Rummaan <rummaanahmad2001@gmail.com>
2022-04-29 10:04:48 +0530

    steganography

--- GraphicalAuthentication/HomePage/urls.py
+++ GraphicalAuthentication/HomePage/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home-page'),
     path('admin/', admin.site.urls),
     path('sign-up', views.signup, name='Sign-up'),
+    path('dum', views.dum, name='Dum'),
 ]
